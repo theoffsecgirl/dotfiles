6b66d5d08f1ed441fa51dd761ddfc9b2dbca108c
Arianne – Así me llaman 🦂 <arianne@theoffsecgirl.com>
2025-07-24 23:04:34 +0200

    Update setup_zsh.sh

--- terminal/zsh/setup_zsh.sh
+++ terminal/zsh/setup_zsh.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-USER_HOME="$HOME"
+REAL_USER=$(logname)
+USER_HOME="/home/$REAL_USER"
 ROOT_HOME="/root"
 DOTFILES="$USER_HOME/dotfiles/terminal/zsh"
 ZSH_CUSTOM="$USER_HOME/.zsh"
@@ -32,6 +33,11 @@ if [ ! -d "$ROOT_ZSH_CUSTOM/powerlevel10k" ]; then
   echo -e "$OK Powerlevel10k instalado para root"
 fi
 
+# Copiar config .p10k.zsh por defecto
+cp "$DOTFILES/themes/.p10k.zsh" "$USER_HOME/.p10k.zsh"
+sudo cp "$USER_HOME/dotfiles/terminal/zsh/themes/root/.p10k.zsh" "$ROOT_HOME/.p10k.zsh"
+echo -e "$OK Copiados .p10k.zsh para user y root"
+
 echo -e "$INFO Configurando .zshrc de usuario..."
 USER_ZSHRC="$USER_HOME/.zshrc"
 touch "$USER_ZSHRC"
@@ -46,20 +52,23 @@ add_source_user "exports.zsh"
 add_source_user "aliases.zsh"
 add_source_user "functions.zsh"
 add_source_user "prompt.zsh"
-add_source_user "themes/.p10k.zsh"
 
-P10K_LINE="[[ -f \"$ZSH_CUSTOM/powerlevel10k/powerlevel10k.zsh-theme\" ]] && source \"$ZSH_CUSTOM/powerlevel10k/powerlevel10k.zsh-theme\""
+# Enlazar .p10k.zsh desde home
+P10K_LINE='[[ -f "$HOME/.p10k.zsh" ]] && source "$HOME/.p10k.zsh"'
 grep -qxF "$P10K_LINE" "$USER_ZSHRC" || echo "$P10K_LINE" >> "$USER_ZSHRC"
+
+P10K_THEME_LINE="[[ -f \"$ZSH_CUSTOM/powerlevel10k/powerlevel10k.zsh-theme\" ]] && source \"$ZSH_CUSTOM/powerlevel10k/powerlevel10k.zsh-theme\""
+grep -qxF "$P10K_THEME_LINE" "$USER_ZSHRC" || echo "$P10K_THEME_LINE" >> "$USER_ZSHRC"
 
 echo -e "$OK .zshrc de usuario listo"
 
 echo -e "$INFO Configurando .zshrc de root..."
 sudo tee $ROOT_HOME/.zshrc > /dev/null <<EOF
-[[ -f "$DOTFILES/exports.zsh" ]] && source "$DOTFILES/exports.zsh"
-[[ -f "$DOTFILES/aliases.zsh" ]] && source "$DOTFILES/aliases.zsh"
-[[ -f "$DOTFILES/functions.zsh" ]] && source "$DOTFILES/functions.zsh"
-[[ -f "$DOTFILES/prompt.zsh" ]] && source "$DOTFILES/prompt.zsh"
-[[ -f "$DOTFILES/themes/root/.p10k.zsh" ]] && source "$DOTFILES/themes/root/.p10k.zsh"
+[[ -f "/home/$REAL_USER/dotfiles/terminal/zsh/exports.zsh" ]] && source "/home/$REAL_USER/dotfiles/terminal/zsh/exports.zsh"
+[[ -f "/home/$REAL_USER/dotfiles/terminal/zsh/aliases.zsh" ]] && source "/home/$REAL_USER/dotfiles/terminal/zsh/aliases.zsh"
+[[ -f "/home/$REAL_USER/dotfiles/terminal/zsh/functions.zsh" ]] && source "/home/$REAL_USER/dotfiles/terminal/zsh/functions.zsh"
+[[ -f "/home/$REAL_USER/dotfiles/terminal/zsh/prompt.zsh" ]] && source "/home/$REAL_USER/dotfiles/terminal/zsh/prompt.zsh"
+[[ -f "$ROOT_HOME/.p10k.zsh" ]] && source "$ROOT_HOME/.p10k.zsh"
 [[ -f "$ROOT_ZSH_CUSTOM/powerlevel10k/powerlevel10k.zsh-theme" ]] && source "$ROOT_ZSH_CUSTOM/powerlevel10k/powerlevel10k.zsh-theme"
 EOF
 
@@ -70,3 +79,4 @@ chsh -s "$(which zsh)"
 sudo chsh -s "$(which zsh)" root
 
 echo -e "$OK Instalación completada. Reinicia la terminal o ejecuta: exec zsh"
+
